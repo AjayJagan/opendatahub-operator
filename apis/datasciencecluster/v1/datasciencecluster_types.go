ac1d6db8d34285e98e065a9d9cc901f09bc6451e
AJAY_JAGANATHAN <ajayjeganathan@gmail.com>
2024-03-12 18:46:08 +0530

    test

--- apis/datasciencecluster/v1/datasciencecluster_types.go
+++ apis/datasciencecluster/v1/datasciencecluster_types.go
@@ -81,7 +81,6 @@ type Components struct {
 	ModelRegistry modelregistry.ModelRegistry `json:"modelregistry,omitempty"`
 
 	TestComponent string `json:"test,omitempty"`
-	ModelServer   string `json:"modelserver,omitempty"`
 }
 
 // DataScienceClusterStatus defines the observed state of DataScienceCluster.
