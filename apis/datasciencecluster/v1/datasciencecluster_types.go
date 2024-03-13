294ae8c7717970a22638e3e6a376cf7a06b34460
AJAY_JAGANATHAN <ajayjeganathan@gmail.com>
2024-03-13 22:40:56 +0530

    test

--- apis/datasciencecluster/v1/datasciencecluster_types.go
+++ apis/datasciencecluster/v1/datasciencecluster_types.go
@@ -103,6 +103,7 @@ type DataScienceClusterStatus struct {
 	// List of components with status if installed or not
 	InstalledComponents map[string]bool `json:"installedComponents,omitempty"`
 	TestComponent       string          `json:"testComponent,omitempty"`
+	Test1Component      string          `json:"test1Component,omitempty"`
 }
 
 //+kubebuilder:object:root=true
