4351949d442a815d8234930aa12d31d61eb2cb09
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-03-23 22:25:21 +0530

    release automation

---
+++ .github/scripts/wait-for-checks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# @param $1 - PR number or URL
+# wait for a bit until pr is created, otherwise it throws an error "no checks reported on the 'odh-release/e2e-test' branch"
+set -euo pipefail
+
+sleep 10
+
+while $(gh pr checks "$1" | grep -q -v 'tide' | grep -q 'pending'); do
+  printf ":stopwatch: PR checks still pending, retrying in 10 seconds...\n"
+  sleep 10
+done
+
+if $(gh pr checks "$1" | grep -q 'fail'); then
+  printf "!!PR checks failed!!\n"
+  exit 1
+fi
+
+if $(gh pr checks "$1" | grep  -q 'pass'); then
+  printf "!!PR checks passed!!\n"
+  exit 0
+fi
+
+printf "!!An unknown error occurred!!\n"
+exit 1
