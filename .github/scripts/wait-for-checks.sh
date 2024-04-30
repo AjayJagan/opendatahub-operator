19d5e4b350ecd17b43d67965b42a288186d3a607
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-04-30 20:21:59 +0530

    new-updates

--- .github/scripts/wait-for-checks.sh
+++ .github/scripts/wait-for-checks.sh
@@ -2,24 +2,24 @@
 #
 # @param $1 - PR number or URL
 # wait for a bit until pr is created, otherwise it throws an error "no checks reported on the 'odh-release/e2e-test' branch"
-set -euo pipefail
+set -euo
 
 sleep 10
 
-while $(gh pr checks "$1" | grep -q -v 'tide' | grep -q 'pending'); do
-  printf ":stopwatch: PR checks still pending, retrying in 10 seconds...\n"
-  sleep 10
+pr_has_status() {
+    local pr=$1
+    local status=$2
+    local skip=${3:-tide}
+
+    gh pr checks $pr | awk -v FS=$'\t' -v status=$status "\$1 ~ /$skip/{next} \$2 == status {found=1} END {if (!found) exit 1}"
+}
+
+while pr_has_status $1 pending; do
+  echo "PR checks still pending, retrying in 10 seconds..."
+  sleep 30 # replace with 60000
 done
 
-if $(gh pr checks "$1" | grep -q 'fail'); then
-  printf "!!PR checks failed!!\n"
-  exit 1
-fi
-
-if $(gh pr checks "$1" | grep  -q 'pass'); then
-  printf "!!PR checks passed!!\n"
-  exit 0
-fi
-
-printf "!!An unknown error occurred!!\n"
+pr_has_status $1 fail && { echo "!!PR checks failed!!"; exit 1; }
+pr_has_status $1 pass && { echo "!!PR checks passed!!"; exit 0; }
+echo "!!An unknown error occurred!!"
 exit 1
