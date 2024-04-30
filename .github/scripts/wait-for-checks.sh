9b827ae43d18ffbf3f16d552034eea8069804f2e
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-04-30 20:21:59 +0530

    new-updates

--- .github/scripts/wait-for-checks.sh
+++ .github/scripts/wait-for-checks.sh
@@ -2,24 +2,45 @@
 #
 # @param $1 - PR number or URL
 # wait for a bit until pr is created, otherwise it throws an error "no checks reported on the 'odh-release/e2e-test' branch"
-set -euo pipefail
+set -euo
 
 sleep 10
 
-while $(gh pr checks "$1" | grep -q -v 'tide' | grep -q 'pending'); do
-  printf ":stopwatch: PR checks still pending, retrying in 10 seconds...\n"
-  sleep 10
+pr_has_status() {
+    local pr=$1
+    local status=$2
+    local skip=${3:-tide}
+
+    gh pr checks $pr | awk -v FS=$'\t' -v status=$status "\$1 ~ /$skip/{next} \$2 == status {found=1} END {if (!found) exit 1}"
+}
+
+while pr_has_status $1 pending; do
+  printf "PR checks still pending, retrying in 10 seconds...\n"
+  sleep 30 # replace with 60000
 done
 
-if $(gh pr checks "$1" | grep -q 'fail'); then
-  printf "!!PR checks failed!!\n"
-  exit 1
+if pr_has_status $1 fail; then
+  printf "!!PR checks failed!!\n" && exit 1
 fi
 
-if $(gh pr checks "$1" | grep  -q 'pass'); then
-  printf "!!PR checks passed!!\n"
-  exit 0
+if pr_has_status $1 pass; then
+  printf "!!PR checks passed!!\n" && exit 0
 fi
 
 printf "!!An unknown error occurred!!\n"
 exit 1
+
+# while gh pr checks "$1" | grep -v tide | awk -v FS=$'\t'  '{print $2}' | grep -q pending ; do
+#   printf "PR checks still pending, retrying in 10 seconds...\n"
+#   sleep 30 # replace with 60000
+# done
+
+# if gh pr checks "$1" | awk -v FS=$'\t'  '{print $2}' | grep -q fail; then
+#   printf "!!PR checks failed!!\n"
+#   exit 1
+# fi
+
+# if gh pr checks "$1" | awk -v FS=$'\t'  '{print $2}' | grep -q pass; then
+#   printf "!!PR checks passed!!\n"
+#   exit 0
+# fi
