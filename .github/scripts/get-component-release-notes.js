b0eb588eb2211bedd1e0e1001e34599d300b727a
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-09-25 21:04:50 +0530

    test release

--- .github/scripts/get-component-release-notes.js
+++ .github/scripts/get-component-release-notes.js
@@ -1,16 +1,74 @@
 function getModifiedComponentName(name){
-    let modifiedWord = name.split("-").join(" ").replace(/[^a-zA-Z ]/g, "").trim()
+    let modifiedWord = name.split("-").join(" ")
     modifiedWord = modifiedWord[0].toUpperCase() + modifiedWord.slice(1).toLowerCase()
     return modifiedWord.replace("Odh", "ODH")
 }
-module.exports = ({ github, core }) => {
-    const { TRACKER_URL } = process.env
+module.exports = async ({ github, core }) => {
+    const { TRACKER_URL, VERSION } = process.env
     console.log(`The TRACKER_URL is ${TRACKER_URL}`)
     const arr = TRACKER_URL.split("/")
     const owner = arr[3]
     const repo = arr[4]
     const issue_number = arr[6]
 
+    try {
+        const latestReleaseResult = github.request('GET /repos/{owner}/{repo}/releases/latest', {
+            owner,
+            repo,
+            issue_number,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github.text+json'
+            }
+        })
+        const latestTag = latestReleaseResult.data["tag_name"]
+        console.log(`The current tag is: ${latestTag}`)
+
+        const releaseNotesResult = github.request('POST /repos/{owner}/{repo}/releases/generate-notes', {
+            owner,
+            repo,
+            tag_name: VERSION,
+            previous_tag_name: latestTag,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github.text+json'
+            }
+        })
+        const releaseNotesString = releaseNotesResult.data["body"]
+        const commentsResult = github.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+            owner,
+            repo,
+            issue_number,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github.text+json'
+            }
+        })
+        let outputStr = "## Component Release Notes\n"
+        commentsResult.data.forEach((issue) => {
+            let issueCommentBody = issue.body_text
+            if (issueCommentBody.includes("#Release#")) {
+                let components = issueCommentBody.split("\n")
+                const releaseIdx = components.indexOf("#Release#")
+                components = components.splice(releaseIdx + 1, components.length)
+                const regex = /\s*[A-Za-z-_0-9]+\s*\|\s*(https:\/\/github\.com\/.*(tree|releases).*){1}\s*\|?\s*(https:\/\/github\.com\/.*releases.*)?\s*/;
+                components.forEach(component => {
+                    if (regex.test(component)) {
+                        let [componentName, branchUrl, tagUrl] = component.split("|")
+                        componentName = componentName.trim()
+                        const releaseNotesUrl = (tagUrl || branchUrl).trim();
+                        outputStr += `- **${getModifiedComponentName(componentName)}**: ${releaseNotesUrl}\n`
+        
+                    }
+                })
+            }
+        })
+        outputStr += releaseNotesString
+        console.log("Created component release notes successfully...")
+        core.setOutput('release-notes-body', outputStr);
+    }catch(error){
+        core.setFailed(`Action failed with error ${e}`);
+    }
     github.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
         owner,
         repo,
@@ -31,14 +89,15 @@ module.exports = ({ github, core }) => {
                 components.forEach(component => {
                     if (regex.test(component)) {
                         let [componentName, branchUrl, tagUrl] = component.split("|")
-                        componentName = getModifiedComponentName(componentName.trim())
+                        componentName = componentName.trim()
                         const releaseNotesUrl = (tagUrl || branchUrl).trim();
-                        if(!outputStr.includes(componentName)) outputStr += `- **${componentName}**: ${releaseNotesUrl}\n`
+                        outputStr += `- **${getModifiedComponentName(componentName)}**: ${releaseNotesUrl}\n`
         
                     }
                 })
             }
         })
+        outputStr += releaseNotesString
         console.log("Created component release notes successfully...")
         core.setOutput('release-notes-body', outputStr);
     }).catch(e => {
