aebdb68a241ef90935ca1f1f930bfbcb01a1a84e
AJAY JAGANATHAN <ajaganat@redhat.com>
2025-01-21 07:48:12 +0530

    test release

--- .github/scripts/update-manifests-tags.sh
+++ .github/scripts/update-manifests-tags.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 
 update_tags(){
-    sed  -i -r "/\"$1\"/s|([^:]*):([^:]*):[^:]*:(.*)|\1:\2:$2:\3|" get_all_manifests.sh
+    component=$(basename "$1")
+    echo "$component"
+    sed -i -r "/\"(.*\/)*$component\"/s|([^:]*):([^:]*):[^:]*:(.*)|\1:\2:$2:\3|" get_all_manifests.sh
 }
 
 prefix=component_spec_
@@ -12,6 +14,6 @@ echo
 env | while IFS="=" read varname value; do
     [[ $varname =~ "${prefix}" ]] || continue
     component=${varname#${prefix}}
-    component=${component/_/-}
+    component=${component//_/-}
     update_tags $component $value
 done
